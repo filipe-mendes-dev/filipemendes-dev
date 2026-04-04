54a39076262d7ef6743f6d461235c37d3394934c
Flaip <flaip10@gmail.com>
2026-04-04 20:50:37 +0100

    style(ProjectCard): standardize import statements and update link text for clarity

--- src/views/LandingPage/sections/ProjectsSection/components/ProjectCard/ProjectCard.tsx
+++ src/views/LandingPage/sections/ProjectsSection/components/ProjectCard/ProjectCard.tsx
@@ -1,11 +1,11 @@
-import type { ReactElement } from 'react';
+import type { ReactElement } from "react";
 
-import { ExternalLinkIcon } from '../../../../../../components/icons';
-import { TextActionLink } from '../../../../../../components/navigation/TextActionLink';
-import { ProjectLogoMark } from '../../../../../../components/projects/ProjectLogoMark';
-import { SoftSurface } from '../../../../../../components/ui/SoftSurface';
-import type { ProjectCardProps } from './ProjectCard.interfaces';
-import st from './ProjectCard.module.css';
+import { ExternalLinkIcon } from "../../../../../../components/icons";
+import { TextActionLink } from "../../../../../../components/navigation/TextActionLink";
+import { ProjectLogoMark } from "../../../../../../components/projects/ProjectLogoMark";
+import { SoftSurface } from "../../../../../../components/ui/SoftSurface";
+import type { ProjectCardProps } from "./ProjectCard.interfaces";
+import st from "./ProjectCard.module.css";
 
 export const ProjectCard = ({ project }: ProjectCardProps): ReactElement => {
   return (
@@ -31,8 +31,12 @@ export const ProjectCard = ({ project }: ProjectCardProps): ReactElement => {
       <p className={st.description}>{project.description}</p>
 
       <div className={st.actions}>
-        <TextActionLink href={project.href} className={st.link} trailingIcon={<ExternalLinkIcon />}>
-          View Product Details
+        <TextActionLink
+          href={project.href}
+          className={st.link}
+          trailingIcon={<ExternalLinkIcon />}
+        >
+          View Project Details
         </TextActionLink>
       </div>
     </SoftSurface>
