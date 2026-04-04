c9de8e4a6e56944dae71d9041bb41854bebd559e
Flaip <flaip10@gmail.com>
2026-04-04 22:02:59 +0100

    style(DetailBulletList): enhance layout with new index display and responsive design adjustments

--- src/views/ProjectPages/ProjectDetailPage/components/DetailBulletList/DetailBulletList.tsx
+++ src/views/ProjectPages/ProjectDetailPage/components/DetailBulletList/DetailBulletList.tsx
@@ -8,9 +8,14 @@ export const DetailBulletList = ({ items }: DetailBulletListProps): ReactElement
   return (
     <div className={st.root}>
       <ul className={`${su.stackList} ${st.list}`}>
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li className={st.item} key={item}>
-            <p className={st.copy}>{item}</p>
+            <span aria-hidden="true" className={st.index}>
+              {(index + 1).toString().padStart(2, '0')}
+            </span>
+            <div className={st.body}>
+              <p className={st.copy}>{item}</p>
+            </div>
           </li>
         ))}
       </ul>
