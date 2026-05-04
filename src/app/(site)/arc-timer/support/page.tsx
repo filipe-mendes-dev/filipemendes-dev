8dab842ebb34b5cae1adeafe0c85474b0a7c8a4e
Filipe <92937077+filipe-mendes-dev@users.noreply.github.com>
2026-05-04 01:43:19 +0100

    feat(ArcTimerSupport): add support page with metadata, layout, and styling for user assistance

---
+++ src/app/(site)/arc-timer/support/page.tsx
@@ -0,0 +1,10 @@
+import type { ReactElement } from "react";
+
+import { ArcTimerSupportPage } from "../../../../views/ArcTimerSupportPage";
+export { metadata } from "./metadata";
+
+const ArcTimerSupportRoute = (): ReactElement => {
+  return <ArcTimerSupportPage />;
+};
+
+export default ArcTimerSupportRoute;
---
+++ src/views/ArcTimerSupportPage/index.ts
@@ -0,0 +1,1 @@
+export { ArcTimerSupportPage } from "./ArcTimerSupportPage";
